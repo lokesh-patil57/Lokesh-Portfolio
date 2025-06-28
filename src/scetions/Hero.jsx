dab0a222c05f038a92e5a9820cbcc2907b12340e
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-28 09:49:44 +0530

    Button And Bio added

--- src/scetions/Hero.jsx
+++ src/scetions/Hero.jsx
@@ -1,7 +1,7 @@
 import { space } from "postcss/lib/list";
 import React from "react";
 import { words } from "../constants";
-
+import Button from "../components/Button";
 
 const Hero = () => {
   return (
@@ -14,21 +14,38 @@ const Hero = () => {
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text ">
-              <h1>Shaping 
+              <h1>
+                Shaping
                 <span className="slide">
-                     <span className="wrapper" >
-                        {words.map((word) => (
-                            <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2" >
-                                <img src={word.imgPath} alt={word.text} className="xl:size-12  md:size-10 size-7 md :p-2 p-1 rounded-full bg-white-50" />
-                                <span>{word.text}</span>
-                            </span>
-                        ))}
-                     </span>
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12  md:size-10 size-7 md :p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
                 </span>
               </h1>
               <h1>into Real Project</h1>
               <h1>that deliever Results</h1>
             </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none ">
+              Hi, I am Lokesh , A Developer based in India with a Passion for
+              Code
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my Work"
+            />
           </div>
         </header>
         {/*RIGHT : 3D MODEL */}
