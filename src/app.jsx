219624f9e650b5809a96474f9acd93f9b991d060
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-28 07:53:40 +0530

    Initial changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,10 @@
+import Hero from './scetions/Hero'
 
 const App = () => {
   return (
-    <main>
-      <div>
-        <h1>Welcome To my 3D Portfolio</h1>
-      </div>
-    </main>
+    <>
+    <Hero/>
+    </>
   )
 }
 
---
+++ src/scetions/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='hero' className='relative overflow-hidden'>
+        <div className="absolute top-0 left-0 z-10">
+            <img src="/images/bg.png" alt="background" />
+        </div>
+        <div className="hero-layout">
+            {/*LEFT : HERO CONTENT */}
+        </div>
+      Hero
+    </section>
+  )
+}
+
+export default Hero
+ 
