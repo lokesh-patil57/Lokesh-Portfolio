109616e1952503966d2e5a8c6902a42cfb225f38
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-28 09:28:41 +0530

    Left side Hero content

--- src/scetions/Hero.jsx
+++ src/scetions/Hero.jsx
@@ -13,12 +13,12 @@ const Hero = () => {
         {/*LEFT : HERO CONTENT */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
-            <div className="hero-tex">
+            <div className="hero-text ">
               <h1>Shaping 
                 <span className="slide">
                      <span className="wrapper" >
                         {words.map((word) => (
-                            <span key={word.text} className="flex iteams-center md:gap-3 gap-1 pb-2" >
+                            <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2" >
                                 <img src={word.imgPath} alt={word.text} className="xl:size-12  md:size-10 size-7 md :p-2 p-1 rounded-full bg-white-50" />
                                 <span>{word.text}</span>
                             </span>
