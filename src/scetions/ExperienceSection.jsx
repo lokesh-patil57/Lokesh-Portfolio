13a7cf7715c0cb97bedc50756d6a51c50814f29d
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-30 20:29:21 +0530

    position tracking of mouse done on glowcard component

--- src/scetions/ExperienceSection.jsx
+++ src/scetions/ExperienceSection.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import TitleHeader from '../components/TitleHeader'
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+import { expCards } from "../constants";
+import GlowCard from "../components/GlowCard";
 
 const ExperienceSection = () => {
   return (
-    <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
+    <section
+      id="experience"
+      className="w-full md:mt-40 mt-20 section-padding xl:px-0"
+    >
       <div className="w-full h-full md:px-20 px-5">
-        <TitleHeader title="Professional Work Experience" sub="💼My Career Overview"/>
+        <TitleHeader
+          title="Professional Work Experience"
+          sub="💼My Career Overview"
+        />
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space y-10">
+            {expCards.map((card , index) => (
+              <div key={card.title} className="exp-card-wrapper">
+                <div className="xl:w-2/6 ">
+                <GlowCard card={card} index={index}>
+                   <div>
+                    <img src={card.imgPath} alt={card.title} /></div> 
+                </GlowCard>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ExperienceSection
+export default ExperienceSection;
