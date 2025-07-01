b815c7e0a2f107be7c2529236533ae1725f599f8
lokesh-patil57 <luckypatil577R@gmail.com>
2025-07-01 08:50:00 +0530

    initiating animation of icon box

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -1,27 +1,42 @@
-import { Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import { DirectionalLight } from 'three'
+import { Environment, Float, OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { DirectionalLight } from "three";
+import { useEffect } from "react";
+import * as THREE from 'three'
 
-const TechIcon = ({model}) => {
-    const scene = useGLTF(model.modelPath)
+const TechIcon = ({ model }) => {
+  const scene = useGLTF(model.modelPath);
+
+  useEffect(() => {
+    if (model.name === "Interactive Developer") {
+      scene.scene.traverse((child) => {
+        if (child.isMesh) {
+          if (child.name === "Object_5") {
+            child.material = new THREE.MeshStandardMaterial({ color: "white" });
+          }
+        }
+      });
+    }
+  }, [scene]);
+
   return (
     <Canvas>
-      <ambientLight intensity={0.3}/>
+      <ambientLight intensity={0.3} />
 
-        <directionalLight position={[5,5,5]} intensity={1}/>
+      <directionalLight position={[5, 5, 5]} intensity={1} />
 
-      <Environment preset='city'/>
+      <Environment preset="city" />
 
-    <OrbitControls enableZoom={false}/>
+      <OrbitControls enableZoom={false} />
 
       <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
         <group scale={model.scale} rotation={model.roetation}>
-            <primitive object={scene.scene}/>
+          <primitive object={scene.scene} />
         </group>
       </Float>
     </Canvas>
-  )
-}
+  );
+};
 
-export default TechIcon
+export default TechIcon;
