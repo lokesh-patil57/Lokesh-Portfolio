659f0473381ce8d9ad8fd5e57bc75bfdddc56d4f
lokesh-patil57 <luckypatil577R@gmail.com>
2025-07-01 07:32:08 +0530

    loading the 3d models

--- src/scetions/TechStack.jsx
+++ src/scetions/TechStack.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import TitleHeader from '../components/TitleHeader'
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+import { techStackIcons } from "../constants";
+import TechIcon from "../components/Models/TechLogos/TechIcon";
 
 const TechStack = () => {
   return (
-    <div id='skills' className='flex-center section-padding'>
-        <div>
-            <TitleHeader
-                title='My Preferred Tech Stack'
-                sub='🤝 The Skills I Bring to the Table'
-            />
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="My Preferred Tech Stack"
+          sub="🤝 The Skills I Bring to the Table"
+        />
+        <div className="text-grid">
+          {techStackIcons.map((icon) => (
+            <div
+              key={icon.name}
+              className="card-border tect-card overflow-hidden group xl:rounded-full rounded-lg"
+            >
+              <div className="tech-card-animated-bg" />
+              <div className="tech-card-content">
+                <div className="tech-icon-wrapper">
+                    <TechIcon model={icon}/>
+                </div>
+                <div className="padding-x w-full">
+                    <p>{icon.name}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TechStack
+export default TechStack;
