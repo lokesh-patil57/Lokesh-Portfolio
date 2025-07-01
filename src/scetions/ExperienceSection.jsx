65150075da70de4da6a54eab85c5e982bdec101b
lokesh-patil57 <luckypatil577R@gmail.com>
2025-07-01 06:54:45 +0530

    Experience line moving animation done

--- src/scetions/ExperienceSection.jsx
+++ src/scetions/ExperienceSection.jsx
@@ -25,7 +25,20 @@ const ExperienceSection = () => {
             })
         })
 
-        // gsap.from(card)
+        gsap.to('.timeline', {
+          transformOrigin: 'bottom bottom',
+          ease: 'power1.inOut',
+          scrollTrigger:{
+            trigger:'.timeline',
+            start:'top center',
+            end: '70% center',
+            onUpdate:(self) =>{
+              gsap.to('.timeline',{
+                scaleY:1-self.progress
+              })
+            }
+          },
+        })
     },[])
   return (
     <section
