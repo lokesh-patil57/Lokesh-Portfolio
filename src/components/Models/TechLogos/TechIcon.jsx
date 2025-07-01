a4263e35890001618cd8fe05c302f70a927311df
lokesh-patil57 <luckypatil577R@gmail.com>
2025-07-01 07:46:45 +0530

    movement of icon done

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -1,10 +1,26 @@
+import { Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import { DirectionalLight } from 'three'
 
-const TechIcon = () => {
+const TechIcon = ({model}) => {
+    const scene = useGLTF(model.modelPath)
   return (
-    <div>
-      ok
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.3}/>
+
+        <directionalLight position={[5,5,5]} intensity={1}/>
+
+      <Environment preset='city'/>
+
+    <OrbitControls enableZoom={false}/>
+
+      <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
+        <group scale={model.scale} rotation={model.roetation}>
+            <primitive object={scene.scene}/>
+        </group>
+      </Float>
+    </Canvas>
   )
 }
 
