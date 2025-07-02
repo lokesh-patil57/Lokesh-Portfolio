3eb7e59c9878c2726c4971d96fe2d10139087e07
lokesh-patil57 <luckypatil577R@gmail.com>
2025-07-02 08:51:32 +0530

    contact experience for the contact form

---
+++ src/components/Models/Contact/ContactExperience.jsx
@@ -0,0 +1,29 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+import Computer from "./Computer";
+
+const ContactExperience = () => {
+  return (
+    <Canvas shadows camera={{ position: [0, 3, 7], fov: 45 }}>
+      <ambientLight intensity={0.5} color="#fff4e6" />
+
+      <directionalLight position={[5, 5, 3]} intensity={2.5} color="#ffd9b3" />
+
+      <directionalLight
+        position={[5, 9, 1]}
+        castShadow
+        intensity={2.5}
+        color="#ffd9b3"
+      />
+
+      
+
+      <group scale={0.03} position={[0, -1.49, -2]} castShadow>
+        <Computer />
+      </group>
+    </Canvas>
+  );
+};
+
+export default ContactExperience;
