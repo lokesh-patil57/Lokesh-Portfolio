a5395672ed453e25680006be6bbc2634dfc53e40
lokesh-patil57 <luckypatil577R@gmail.com>
2025-07-02 09:28:20 +0530

    footer completed

--- src/scetions/Footer.jsx
+++ src/scetions/Footer.jsx
@@ -5,14 +5,21 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='footer-container'>
-        <div className="flex flex-col justify-center ">
+        <div className="flex flex-col justify-center md:items-start items-center">
             <a href="/">Visit my Blog</a>
         </div>
         <div className='socials'>
             {socialImgs.map((img)=>(
-                <a href={img.imgPath}></a>
+                <a className='icon' target ='_blank' href={img.url} key={img.url}>
+                    <img src={img.imgPath} alt="social images" />
+                </a>
             ))}
         </div>
+        <dic className="flex flex-col justify-center">
+            <p className="text-center md:text-end ">
+                © {new Date().getFullYear()} Lokesh | Patil. All rights reserved.
+            </p>
+        </dic>
       </div>
     </footer>
   )
