854e5aeb12b12052dae72ae8a2179e2f2cb61152
lokesh-patil57 <luckypatil577R@gmail.com>
2025-07-04 21:23:28 +0530

    optimized the code for performance

--- src/components/TitleHeader.jsx
+++ src/components/TitleHeader.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-
-const TitleHeader = ({title,sub}) => {
+const TitleHeader = ({ title, sub }) => {
   return (
-    <div className='flex flex-col items-center gap-5'>
-      <div className='hero-badge'>
+    <div className="flex flex-col items-center gap-5">
+      <div className="hero-badge">
         <p>{sub}</p>
       </div>
-      <div className="font-semibold md:text-5xl text-3xl text-center">
-        {title}
+      <div>
+        <h1 className="font-semibold md:text-5xl text-3xl text-center">
+          {title}
+        </h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleHeader
+export default TitleHeader;
--- src/scetions/Socials.jsx
+++ src/scetions/Socials.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import TitleHeader from '../components/TitleHeader'
-import { socials } from '../constants'
-import GlowCard from '../components/GlowCard'
+import { socials } from "../constants";
+import TitleHeader from "../components/TitleHeader";
+import GlowCard from "../components/GlowCard";
 
 const Socials = () => {
   return (
-    <section  id='socials' className='flex-center section-padding'>
-        <div className="w-full h-full md:px-10 px-5">
-            <TitleHeader title='What I Contributed in hackathon !'
-            sub='⭐ My Hackathon Experiences'/>
-            <div className="lg:columns-3 md:columns-2 columns-1 mt-16">
-                {socials.map(({imgPath , name ,mentions , review})=>(
-                    <GlowCard card={{review}}>
-                       <div className="flex items-center gap-3">
-                        <div>
-                          <img src={imgPath} alt={name} />
-                        </div>
-                        <div >
-                          <p className='font-bold'>{name}</p>
-                          <p className="text-white-50">{mentions}</p>
-                        </div>
-                       </div>
-                    </GlowCard>
-                ))}
-            </div>
-        </div>
-    </section>
-  )
-}
+    <section id="socials" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="What I Contributed in hackathon !"
+          sub="⭐ My Hackathon Experiences"
+        />
 
-export default Socials
+        <div className="lg:columns-3 md:columns-2 columns-1 mt-16">
+          {socials.map((testimonial, index) => (
+            <GlowCard card={testimonial} key={index} index={index}>
+              <div className="flex items-center gap-3">
+                <div>
+                  <img src={testimonial.imgPath} alt="" />
+                </div>
+                <div>
+                  <p className="font-bold">{testimonial.name}</p>
+                  <p className="text-white-50">{testimonial.mentions}</p>
+                </div>
+              </div>
+            </GlowCard>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Socials;
