729acf23e1df927fe185f2198b37fd8f372b0ba2
lokesh-patil57 <luckypatil577R@gmail.com>
2025-07-04 12:32:07 +0530

    Socials added

--- src/scetions/Socials.jsx
+++ src/scetions/Socials.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
-import { testimonials } from '../constants'
+import { socials } from '../constants'
 import GlowCard from '../components/GlowCard'
 
-const Testimonials = () => {
+const Socials = () => {
   return (
-    <section  id='testimonials' className='flex-center section-padding'>
+    <section  id='socials' className='flex-center section-padding'>
         <div className="w-full h-full md:px-10 px-5">
-            <TitleHeader title='What People Say About Me ?'
-            sub='⭐ Client Feedback Highlights'/>
+            <TitleHeader title='What I Contributed in hackathon !'
+            sub='⭐ My Hackathon Experiences'/>
             <div className="lg:columns-3 md:columns-2 columns-1 mt-16">
-                {testimonials.map(({imgPath , name ,mentions , review})=>(
+                {socials.map(({imgPath , name ,mentions , review})=>(
                     <GlowCard card={{review}}>
                        <div className="flex items-center gap-3">
                         <div>
@@ -29,4 +29,4 @@ const Testimonials = () => {
   )
 }
 
-export default Testimonials
+export default Socials
